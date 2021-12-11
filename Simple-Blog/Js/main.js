a2175bff9ab99ddc18919657b875ec0aab933fa8
Mohammad Lotfi <87372623+pedramcvx1@users.noreply.github.com>
2021-12-11 17:00:03 +0200

    CSS,HTML,JS

---
+++ Simple-Blog/Js/main.js
@@ -0,0 +1,62 @@
+//Defining Action
+let toggle = document.querySelector("#header .toggle-button");
+
+//Cause we have 2 collapse div tab
+let collapse = document.querySelectorAll("#header .collapse");
+
+//Returning the result from clicking on the hamburger menu
+toggle.addEventListener('click' , function(){
+    collapse.forEach(col => col.classList.toggle("collapse-toggle"));
+})
+
+// with masonry
+new Masonry("#posts .grid", {
+    itemSelector : '.grid-item',
+    gutter : 20
+});
+
+// swiper libray initialization
+new Swiper('.swiper-container', {
+    direction : 'horizontal',
+    loop : true,
+    slidesPerView : 5,
+    autoplay : {
+        delay : 3000
+    },
+    // responsive bracket points
+    breakpoints : {
+        '@0' : {
+            slidesPerView : 2
+        },
+        // 888px
+        '@1.00' : {
+            slidesPerView : 3
+        },
+        // 1110px
+        '@1.25' : {
+            slidesPerView : 4
+        },
+        // 1330px
+        '@1.50' : {
+            slidesPerView: 5
+        }
+    }
+})
+
+// Sticky Navigation
+window.onscroll = function(){ myFunction()};
+
+// get the current value 
+let navbar = document.getElementById("header");
+
+// get the navbar position
+let sticky = navbar.offsetTop;
+
+// sticky function
+function myFunction(){
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+}
